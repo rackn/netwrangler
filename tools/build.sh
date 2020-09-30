4c44aee9c6741fa3c68f66391febb8039ea9905d
Victor Lowther <victor.lowther@gmail.com>
2020-09-30 10:57:37 -0500

    fix(rhel): Fix busted RHEL rendering for GATEWAY.
    Since we have been using IPADDR%d and NETMASK%d this whole time,
    we should also be using GATEWAY0 instead of GATEWAY. Oops.

--- tools/build.sh
+++ tools/build.sh
@@ -10,10 +10,4 @@ elif ! (( ${BASH_REMATCH[1]} > ${WANTED_VER[0]} || ${BASH_REMATCH[2]} >= ${WANTE
     echo "Go Version needs to be ${WANTED_VER[0]}.${WANTED_VER[1]} or higher: currently $GO_VERSION"
     exit -1
 fi
-[[ $GOPATH ]] || export GOPATH="$HOME/go"
-fgrep -q "$GOPATH/bin" <<< "$PATH" || export PATH="$PATH:$GOPATH/bin"
-if ! which glide &>/dev/null; then
-    go get -v github.com/Masterminds/glide
-fi
-glide i
 (cd cmd; go build -o netwrangler)
